4fb80dc8abebd7430c38f671e91c619869d9b0b9
Klaus Büttner <klaus.buettner.git@sos-berlin.com>
2014-07-09 11:58:31 +0000

    -
    svn-id 26234

--- sos-dialog/src/main/java/com/sos/dialog/components/SOSMenuItem.java
+++ sos-dialog/src/main/java/com/sos/dialog/components/SOSMenuItem.java
@@ -1,5 +1,4 @@
 package com.sos.dialog.components;
-
 import java.util.prefs.Preferences;
 
 import org.eclipse.swt.events.SelectionAdapter;
@@ -7,78 +6,67 @@ import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.widgets.Menu;
 import org.eclipse.swt.widgets.MenuItem;
 
- 
- 
 public class SOSMenuItem extends MenuItem {
-    private static final String SOS_SCHEDULER = "SOS_SCHEDULER";
+	private static final String	SOS_SCHEDULER	= "SOS_SCHEDULER";
+	private Preferences			prefs;
+	private String				menuName		= "";
 
-     private Preferences prefs;
-     private String menuName="";
+	public SOSMenuItem(Menu parent, int style, String label) {
+		super(parent, style);
+		initialize(label, false);
+	}
 
-    public SOSMenuItem(Menu parent, int style, String label) {
-        super(parent, style);
-        initialize(label,false); 
-    }
-    
+	public SOSMenuItem(Menu parent, int style, String menuName_, String label) {
+		super(parent, style);
+		this.menuName = menuName_;
+		initialize(label, false);
+	}
 
-    public SOSMenuItem(Menu parent, int style, String menuName_, String label) {
-        super(parent, style);
-        this.menuName = menuName_;
-        initialize(label,false); 
-    }
+	public SOSMenuItem(Menu parent, int style, String menuName_, String label, boolean checked) {
+		super(parent, style);
+		this.menuName = menuName_;
+		initialize(label, checked);
+	}
 
-    public SOSMenuItem(Menu parent, int style, String menuName_, String label, boolean checked) {
-        super(parent, style);
-        this.menuName = menuName_;
-        initialize(label,checked); 
-    }
-    
-   private void initialize(String label,boolean checked) {
-       prefs = Preferences.userNodeForPackage(this.getClass());
+	private void initialize(String label, boolean checked) {
+		prefs = Preferences.userNodeForPackage(this.getClass());
+		setText(label);
+		setSelection(getSelectionFromPrefs(checked));
+		addSelectionListener(new SelectionAdapter() {
+			@Override public void widgetSelected(SelectionEvent e) {
+				prefs.node(SOS_SCHEDULER).put(prefKey(), getSelectionAsString());
+			}
+		});
+	}
 
-       setText(label);
-       setSelection(getSelectionFromPrefs(checked));
-       
+	private String prefKey() {
+		return "MENU_ITEM:" + menuName + ":" + getText() + ":selection";
+	}
 
-       addSelectionListener(new SelectionAdapter() {
-           public void widgetSelected(SelectionEvent e) {
-               prefs.node(SOS_SCHEDULER).put(prefKey(), getSelectionAsString());;
-           }
-       });
-   
-   }
-   private String prefKey() {
-       
-       return "MENU_ITEM:"+ menuName + ":" +getText() + ":selection";
-   }
-    
-   private boolean getSelectionFromPrefs(boolean defaultValue) {
-       String defaultS = "false";
-       if (defaultValue) {
-           defaultS = "true";
-       }
-       String sel = prefs.node(SOS_SCHEDULER).get(prefKey(), defaultS);
-       return sel.equals("true");        
-   }
-   
-   public void setText(String menuName_, String text) {
-       this.menuName = menuName_;
-       super.setText(text);
-   }
-    
-   private String getSelectionAsString() {
-       if (getSelection()) {
-           return "true";
-       }else {
-           return "false";
-       }
-   }
-  
-    
-    protected void checkSubclass() {
+	private boolean getSelectionFromPrefs(boolean defaultValue) {
+		String defaultS = "false";
+		if (defaultValue) {
+			defaultS = "true";
+		}
+		String sel = prefs.node(SOS_SCHEDULER).get(prefKey(), defaultS);
+		return sel.equals("true");
+	}
 
-        // Disable the check that prevents subclassing of SWT components
-    }
+	public void setText(String menuName_, String text) {
+		this.menuName = menuName_;
+		super.setText(text);
+	}
 
+	private String getSelectionAsString() {
+		if (getSelection()) {
+			return "true";
+		}
+		else {
+			return "false";
+		}
+	}
 
+	@Override protected void checkSubclass() {
+		// Disable the check that prevents subclassing of SWT components
+	}
 }
