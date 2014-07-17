eabf0cc7d736735593c1e3ec5a55ace63b879862
Oliver Haufe <oliver.haufe.git@sos-berlin.com>
2014-07-17 11:21:43 +0000

    using wildcards in java classpath
    svn-id 26348

--- sos-net/src/main/java/sos/net/sosftp/sosftp.cmd
+++ sos-net/src/main/java/sos/net/sosftp/sosftp.cmd
@@ -4,7 +4,7 @@
 @rem Company: Software- und Organisations-Service GmbH
 @rem Author : Andreas Püschel <andreas.pueschel@sos-berlin.com>
 @rem Author : Oliver Haufe <oliver.haufe@sos-berlin.com>
-@rem $Id$
+@rem $Id:$
 @rem Purpose: start FTP Processing
 @rem ------------------------------------------------------------
 SETLOCAL
@@ -21,19 +21,7 @@ if not exist "%JAVA_BIN%" set JAVA_BIN=java.exe
 
 cd /D "%CLASSPATH_BASE%"
 
-@rem classpath
-set CP=
-for /F "usebackq" %%i in (`dir /B "*.jar"`) do call :set_classpath "%%i"
-set CP=%CP:~1%
-goto start
-
-:set_classpath
-set CP=%CP%;%~1
-goto final
-
-
-:start
-"%JAVA_BIN%" -classpath "%CP:\=/%" sos.net.SOSFTPCommand %* 
+"%JAVA_BIN%" -classpath "*" sos.net.SOSFTPCommand %*
 set /a RC=%ERRORLEVEL%
 
 cd /D "%CUR_DIR%"
--- sos-net/src/main/java/sos/net/sosftp/sosftp.sh
+++ sos-net/src/main/java/sos/net/sosftp/sosftp.sh
@@ -4,7 +4,7 @@
 # Company: Software- und Organisations-Service GmbH
 # Author : Andreas Püschel <andreas.pueschel@sos-berlin.com>
 # Author : Oliver Haufe <oliver.haufe@sos-berlin.com>
-# $Id$
+# $Id:$
 # Purpose: start FTP Processing
 # ------------------------------------------------------------
 #
@@ -19,15 +19,8 @@ test -z "$JAVA_HOME" && JAVA_HOME="%{JAVA_HOME}"
 JAVA_BIN="$JAVA_HOME/bin/java"
 
 cd "$CLASSPATH_BASE"
-# set_classpath
-CP=""
-for lib in `ls *.jar` 
-do
-  CP="$CP:$lib"
-done
-CP=`echo "$CP" | cut -c 2-`
 
-"$JAVA_BIN" -classpath "$CP" sos.net.SOSFTPCommand "$@" -current_pid=$$ -ppid=$PPID
+"$JAVA_BIN" -classpath "*" sos.net.SOSFTPCommand "$@" -current_pid=$$ -ppid=$PPID
 RC=$?
 
 cd "$CUR_DIR"
