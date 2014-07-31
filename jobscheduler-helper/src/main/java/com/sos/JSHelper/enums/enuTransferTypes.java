7c4aadee7ef7ac7b47b59a448c5171ecdbb2c13a
Klaus Büttner <klaus.buettner.git@sos-berlin.com>
2014-07-31 18:37:40 +0000

    -
    svn-id 26572

--- jobscheduler-helper/src/main/java/com/sos/JSHelper/enums/enuTransferTypes.java
+++ jobscheduler-helper/src/main/java/com/sos/JSHelper/enums/enuTransferTypes.java
@@ -2,25 +2,36 @@
  * 
  */
 package com.sos.JSHelper.enums;
-
-
 /**
  * @author KB
  *
  */
-public  enum enuTransferTypes {
+public enum enuTransferTypes {
+	// @formatter:off
 	local, /* filesystem on localhost */
 	file, /* same as local */
-	ftp, sftp, ftps, ssh2, zip, mq, // Message Queue
-	http, https, svn, webdav, smb, smtp, imap
+	ftp, 
+	sftp, 
+	ftps, 
+	ssh2, 
+	zip, 
+	mq, // Message Queue
+	http, 
+	https, 
+	svn, 
+	webdav, 
+	smb, 
+	smtp, 
+	imap
 	/* */;
+	// @formatter:on
 	public String Text() {
 		String strT = this.name();
 		return strT;
 	}
 
 	public static String[] getArray() {
-		String[] strA = new String[15];
+		String[] strA = new String[enuTransferTypes.values().length];
 		int i = 0;
 		for (enuTransferTypes enuType : enuTransferTypes.values()) {
 			strA[i++] = enuType.Text();
