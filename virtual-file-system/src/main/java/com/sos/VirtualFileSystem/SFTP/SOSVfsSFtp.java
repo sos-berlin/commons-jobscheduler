887a2e10927ab52bae1998c3d1f4eb1e91b72273
Oliver Haufe <oliver.haufe.git@sos-berlin.com>
2015-02-16 14:43:32 +0100

    https://change.sos-berlin.com/browse/JADE-236

--- virtual-file-system/src/main/java/com/sos/VirtualFileSystem/SFTP/SOSVfsSFtp.java
+++ virtual-file-system/src/main/java/com/sos/VirtualFileSystem/SFTP/SOSVfsSFtp.java
@@ -168,7 +168,7 @@ public class SOSVfsSFtp extends SOSVfsBaseClass implements ISOSVfsFileTransfer, 
 	}
 
 	/**
-	 * Creates a new subdirectory on the FTP server in the current directory .
+	 * Creates a directory on the server.
 	 * @param pstrPathName The pathname of the directory to create.
 	 * @return True if successfully completed, false if not.
 	 * @throws java.lang.IOException
@@ -179,25 +179,33 @@ public class SOSVfsSFtp extends SOSVfsBaseClass implements ISOSVfsFileTransfer, 
 			SOSOptionFolderName objF = new SOSOptionFolderName(pstrPathName);
 			reply = "mkdir OK";
 			logger.debug(HostID(SOSVfs_D_179.params("mkdir", pstrPathName)));
-			for (String strSubFolder : objF.getSubFolderArray()) {
+			String[] subfolders = objF.getSubFolderArrayReverse();
+			int idx = subfolders.length;
+			for (String strSubFolder : objF.getSubFolderArrayReverse()) {
 				SFTPv3FileAttributes attributes = getAttributes(strSubFolder);
-				if (attributes == null) {
-					Client().mkdir(strSubFolder, 484);
-//					logger.debug(HostID(SOSVfs_D_135.params("[mkdir]", strSubFolder, getReplyString())));
+				if (attributes != null && attributes.isDirectory()) {
+					logger.debug(SOSVfs_E_180.params(strSubFolder));
+					break;
 				}
-				else {
-					if (attributes.isDirectory() == false) {
-						RaiseException(SOSVfs_E_277.params(strSubFolder));
-					}
+				if (attributes != null && !attributes.isDirectory()) {
+					RaiseException(SOSVfs_E_277.params(strSubFolder));
+					break;
 				}
+				idx--;
 			}
-//			DoCD(strCurrentDir);
+			subfolders = objF.getSubFolderArray();
+			for (int i = idx; i < subfolders.length; i++) {
+				Client().mkdir(subfolders[i], 484);
+				logger.debug(HostID(SOSVfs_E_0106.params("mkdir", subfolders[i], getReplyString())));
+			}
 			logger.debug(HostID(SOSVfs_D_181.params("mkdir", pstrPathName, getReplyString())));
 		}
 		catch (IOException e) {
-			throw new JobSchedulerException(HostID(SOSVfs_E_130.params("makeDirectory()")), e);
+			reply = e.toString();
+			RaiseException(e, SOSVfs_E_134.params("[mkdir]"));
 		}
 	}
+	
 
 	/**
 	 * Removes a directory on the FTP server (if empty).
@@ -205,29 +213,20 @@ public class SOSVfsSFtp extends SOSVfsBaseClass implements ISOSVfsFileTransfer, 
 	 * @throws java.lang.IOException
 	 */
 	@Override
-	public void rmdir(final String pstrPathName) throws IOException {
+	public void rmdir(final String pstrPathName) {
 		try {
 			SOSOptionFolderName objF = new SOSOptionFolderName(pstrPathName);
 			reply = "rmdir OK";
 			for (String subfolder : objF.getSubFolderArrayReverse()) {
 				String strT = subfolder + "/";
-				logger.debug(HostID(SOSVfs_D_135.params("[rmdir]", strT, getReplyString())));
+				logger.debug(HostID(SOSVfs_D_135.params("[rmdir]", subfolder, getReplyString())));
 				Client().rmdir(strT);
 			}
-//			String[] strP = pstrPathName.split("/");
-//			for (int i = strP.length; i > 0; i--) {
-//				String strT = "";
-//				for (int j = 0; j < i; j++) {
-//					strT += strP[j] + "/";
-//				}
-//				logger.debug(HostID(SOSVfs_D_135.params("[rmdir]", strT, getReplyString())));
-//				Client().rmdir(strT);
-//			}
 			reply = "rmdir OK";
 		}
-		catch (Exception e) {
-			// TODO Auto-generated catch block
-			throw new JobSchedulerException(HostID(SOSVfs_E_130.params("removeDirectory()")), e);
+		catch (IOException e) {
+			reply = e.toString();
+			RaiseException(e, SOSVfs_E_134.params("[rmdir]"));
 		}
 	}
 
