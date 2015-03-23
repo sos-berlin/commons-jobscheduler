0681f3988ae193556448a8034157de0178a95b81
Santiago Aucejo <santiago.aucejo.petzoldt.git@sos-berlin.com>
2015-03-23 09:46:38 +0100

    JITL-123: switch added to choose JSch or Trilead SSH Implementation

--- sos-ssh/src/main/java/sos/scheduler/job/SOSSSHJob2JSAdapter.java
+++ sos-ssh/src/main/java/sos/scheduler/job/SOSSSHJob2JSAdapter.java
@@ -5,8 +5,10 @@ import java.util.HashMap;
 import com.sos.JSHelper.Exceptions.JobSchedulerException;
 
 import sos.net.ssh.SOSSSHJob2;
+import sos.net.ssh.SOSSSHJobJSch;
 import sos.net.ssh.SOSSSHJobTrilead;
 import sos.net.ssh.SOSSSHJobOptions;
+import sos.spooler.Variable_set;
 
 public class SOSSSHJob2JSAdapter extends SOSSSHJob2JSBaseAdapter {
   private final String conClassName = this.getClass().getSimpleName();
@@ -32,11 +34,22 @@ public class SOSSSHJob2JSAdapter extends SOSSSHJob2JSBaseAdapter {
   } // spooler_process
   
   private void doProcessing() throws Exception {
-
-    SOSSSHJob2 objR = new SOSSSHJobTrilead();
+    SOSSSHJob2 objR;
+    Variable_set allParams = getGlobalSchedulerParameters();
+    allParams.merge(getParameters());
+    if(allParams.value("jitl.ssh.use_jsch_impl") == null ||
+        allParams.value("jitl.ssh.use_jsch_impl").equalsIgnoreCase("default") ||
+        allParams.value("jitl.ssh.use_jsch_impl").equalsIgnoreCase("false")){
+      //this is the default value for 1.9, will change to JSch with v 1.10 [SP]
+      objR = new SOSSSHJobTrilead();
+      spooler_log.debug9("uses Trilead implementation of SSH");
+    } else {
+      objR = new SOSSSHJobJSch();
+      spooler_log.debug9("uses JSch implementation of SSH");
+    } 
     SOSSSHJobOptions objO = objR.Options();
     objO.CurrentNodeName(this.getCurrentNodeName());
-    HashMap<String, String> hsmParameters1 = getSchedulerParameterAsProperties(getJobOrOrderParameters());
+    HashMap<String, String> hsmParameters1 = getSchedulerParameterAsProperties(allParams);
     objO.setAllOptions(objO.DeletePrefix(hsmParameters1, "ssh_"));
 
     objR.setJSJobUtilites(this);
