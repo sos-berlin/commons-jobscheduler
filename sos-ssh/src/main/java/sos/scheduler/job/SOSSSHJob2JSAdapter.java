505b62b9c09f1523e229f522cf6adeca2e75e1b9
Santiago Aucejo <santiago.aucejo.petzoldt.git@sos-berlin.com>
2015-04-14 18:06:33 +0200

    JITL-147

--- sos-ssh/src/main/java/sos/scheduler/job/SOSSSHJob2JSAdapter.java
+++ sos-ssh/src/main/java/sos/scheduler/job/SOSSSHJob2JSAdapter.java
@@ -16,6 +16,7 @@ import sos.spooler.Variable_set;
 public class SOSSSHJob2JSAdapter extends SOSSSHJob2JSBaseAdapter {
   private final String conClassName = this.getClass().getSimpleName();
   private static final String PARAM_SSH_JOB_TASK_ID = "SSH_JOB_TASK_ID";
+  private static final String PARAM_SSH_JOB_NAME = "SSH_JOB_NAME";
   private static final String PARAM_PIDS_TO_KILL = "PIDS_TO_KILL";
   private static final String PARAM_RUN_WITH_WATCHDOG = "RUN_WITH_WATCHDOG";
   private static final String PARAM_JITL_SSH_USE_JSCH_IMPL = "jitl.ssh.use_jsch_impl";
@@ -45,30 +46,25 @@ public class SOSSSHJob2JSAdapter extends SOSSSHJob2JSBaseAdapter {
   
   private void doProcessing() throws Exception {
     SOSSSHJob2 objR;
-    Variable_set allParams = getGlobalSchedulerParameters();
-    allParams.merge(getParameters());
+//    Variable_set allParams = getGlobalSchedulerParameters();
+//    allParams.merge(getParameters());
+    Variable_set allParams = getParameters();
+    allParams.merge(getGlobalSchedulerParameters());
+    SOSSSHJobOptions objO = null;
     if(allParams.value(PARAM_JITL_SSH_USE_JSCH_IMPL) == null ||
         allParams.value(PARAM_JITL_SSH_USE_JSCH_IMPL).equalsIgnoreCase("default") ||
         allParams.value(PARAM_JITL_SSH_USE_JSCH_IMPL).equalsIgnoreCase("false")){
-      //this is the default value for 1.9, will change to JSch with v 1.10 [SP]
+      //this is the default value for v1.9, will change to JSch with v1.10 [SP]
       useTrilead = true;
       objR = new SOSSSHJobTrilead();
+      objO = objR.Options();
       spooler_log.debug9("uses Trilead implementation of SSH");
     } else {
       useTrilead = false;
       objR = new SOSSSHJobJSch();
-      // generate temporary file for remote pids for further usage
-      pidFileName = generateTempPidFileName();
-      ((SOSSSHJobJSch)objR).setPidFileName(pidFileName);
-      if(allParams.value(PARAM_RUN_WITH_WATCHDOG) != null && 
-          !allParams.value(PARAM_RUN_WITH_WATCHDOG).isEmpty() && 
-          allParams.value(PARAM_RUN_WITH_WATCHDOG).equalsIgnoreCase("true")){
-        createOrderForWatchdog();
-        allParams.set_value(PARAM_PID_FILE_NAME_KEY, pidFileName);
-      }
+      objO = objR.Options();
       spooler_log.debug9("uses JSch implementation of SSH");
     } 
-    SOSSSHJobOptions objO = objR.Options();
     objO.CurrentNodeName(this.getCurrentNodeName());
     HashMap<String, String> hsmParameters1 = getSchedulerParameterAsProperties(allParams);
     objO.setAllOptions(objO.DeletePrefix(hsmParameters1, "ssh_"));
@@ -76,8 +72,18 @@ public class SOSSSHJob2JSAdapter extends SOSSSHJob2JSBaseAdapter {
     if (objO.commandSpecified() == false) {
       setJobScript(objO.command_script);
     }
-
     objO.CheckMandatory();
+    if(!useTrilead){
+      // generate temporary file for remote pids for further usage
+      spooler_log.debug9("Run with watchdog set to: " + objO.runWithWatchdog.Value());
+      if(objO.runWithWatchdog.value()){
+        spooler_log.debug9("option runWithWatchdog is true");
+        pidFileName = generateTempPidFileName();
+        ((SOSSSHJobJSch)objR).setPidFileName(pidFileName);
+        createOrderForWatchdog();
+        allParams.set_value(PARAM_PID_FILE_NAME_KEY, pidFileName);
+      }
+    }
     // if command_delimiter is not set by customer then we override the default value due to compatibility issues
     if(objO.command_delimiter.isNotDirty()){
       objO.command_delimiter.Value(";");
@@ -87,14 +93,27 @@ public class SOSSSHJob2JSAdapter extends SOSSSHJob2JSBaseAdapter {
   
   // creates a new order for the cleanup jobchain with all the options, params, values and the TaskId of the task which created this
   private void createOrderForWatchdog(){
+    spooler_log.debug9("createOrderForWatchdog started");
     Order order = spooler.create_order();
+    order.params().merge(spooler_task.params());
     order.params().merge(spooler_task.order().params());
     order.params().set_var(PARAM_SSH_JOB_TASK_ID, String.valueOf(spooler_task.id()));
     order.params().set_var(PARAM_PID_FILE_NAME_KEY, pidFileName);
+    order.params().set_var(PARAM_SSH_JOB_NAME, spooler_job.name());
     // delayed start after 5 seconds when the order is created
     order.set_at("now+15");
+    //TODO
+//    Job_chain chain;
+//    if(spooler_task.params().value("cleanupJobchain") != null){
+//      chain = spooler.job_chain(spooler_task.params().value("cleanupJobchain"));      
+//      spooler_log.debug9("uses jobchainname from parameter");
+//    }else{//default zum testen
+//      chain = spooler.job_chain("kill_jobs/remote_cleanup_test");
+//      spooler_log.debug9("uses default jobchainname from code");
+//    }
     Job_chain chain = spooler.job_chain("kill_jobs/remote_cleanup_test");
     chain.add_or_replace_order(order);
+    spooler_log.debug9("order send");
   }
   
   private String generateTempPidFileName(){
