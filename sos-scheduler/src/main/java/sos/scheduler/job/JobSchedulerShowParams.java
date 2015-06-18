c5958cfd381ca9bc928cb1bc9c28a308bb6696b6
Santiago Aucejo <santiago.aucejo.petzoldt.git@sos-berlin.com>
2015-06-18 10:03:02 +0200

    JITL-145: Method added to clean Variable_set before logging Variable_set.xml()

--- sos-scheduler/src/main/java/sos/scheduler/job/JobSchedulerShowParams.java
+++ sos-scheduler/src/main/java/sos/scheduler/job/JobSchedulerShowParams.java
@@ -27,7 +27,9 @@ public class JobSchedulerShowParams extends JobSchedulerJob {
 		Variable_set params = spooler_task.params();
 		spooler_log.info("Params for task: " + spooler_task.id());
 		if (params != null) {
-			spooler_log.info("Job params: \n" + params.xml());
+			//JITL-145
+//			spooler_log.info("Job params: \n" + params.xml());
+			spooler_log.info("Job params: \n" + getCleanedParams(params).xml());
 		}
 		Order order = spooler_task.order();
 		Variable_set payload = null;
@@ -37,10 +39,34 @@ public class JobSchedulerShowParams extends JobSchedulerJob {
 				payload = (Variable_set) oPayload;
 		}
 		if (payload != null) {
-			spooler_log.info("Order payload: \n" + payload.xml());
+			//JITL-145
+//			spooler_log.info("Order payload: \n" + payload.xml());
+			spooler_log.info("Order payload: \n" + getCleanedParams(payload).xml());
 		}
 		else
 			spooler_log.info("No order payload.");
 		return rc;
+	}
+	
+	/**
+	 * This method returns the params Variable_set with cleaned up password parameters to prevent security Issues
+	 * 
+	 * @param originalParams the Variable_set which can contain clear type passwords
+	 * @return the new cleansed Variable_set
+	 * @author SP
+	 */
+	private Variable_set getCleanedParams(Variable_set originalParams){
+		Variable_set cleanedParams = spooler.create_variable_set();
+		String names = originalParams.names();
+		java.util.StringTokenizer tokenizer = new java.util.StringTokenizer( originalParams.names(), ";" ); 
+		while (tokenizer.hasMoreTokens()){
+		    String name = tokenizer.nextToken();
+		    if(name.contains("password")){
+		    	cleanedParams.set_value(name, "*****");
+		    }else{
+		    	cleanedParams.set_value(name, originalParams.value(name));
+		    }
+		}
+		return cleanedParams;
 	}
 }
