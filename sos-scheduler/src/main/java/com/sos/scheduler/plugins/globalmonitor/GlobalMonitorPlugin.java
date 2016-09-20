94a91eeb534e7343e141a25654f2d16dc437a553
Santiago Aucejo <santiago.aucejo.petzoldt.git@sos-berlin.com>
2016-09-20 12:07:53 +0200

    Update because of engine merge

--- sos-scheduler/src/main/java/com/sos/scheduler/plugins/globalmonitor/GlobalMonitorPlugin.java
+++ sos-scheduler/src/main/java/com/sos/scheduler/plugins/globalmonitor/GlobalMonitorPlugin.java
@@ -51,7 +51,7 @@ public class GlobalMonitorPlugin extends AbstractPlugin implements XmlConfigurat
     public byte[] changeXmlConfiguration(FileBasedType typ, AbsolutePath path, byte[] xmlBytes) {
         LOGGER.debug("---------  changeXmlConfiguration");
         Document doc = null;
-        if (typ == FileBasedType.job) {
+        if (typ == FileBasedType.Job) {
             doc = xmlBytesToDom(xmlBytes);
         }
         try {
@@ -64,7 +64,7 @@ public class GlobalMonitorPlugin extends AbstractPlugin implements XmlConfigurat
 
     @Override
     public Set<FileBasedType> fileBasedTypes() {
-        return toScalaSet(FileBasedType.job);
+        return toScalaSet(FileBasedType.Job);
     }
 
     private String getStringFromDocument(Document doc) {
