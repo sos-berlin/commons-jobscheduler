21b580cf27ee2241151c55715aaafa8d2be6c601
Oliver Haufe <oliver.haufe@sos-berlin.com>
2016-11-21 14:46:26 +0100

    JOC - xml commands without model marshaling

--- sos-security/src/main/java/com/sos/auth/rest/SOSServicePermissionShiro.java
+++ sos-security/src/main/java/com/sos/auth/rest/SOSServicePermissionShiro.java
@@ -35,8 +35,6 @@ import com.sos.joc.Globals;
 import com.sos.joc.classes.JOCDefaultResponse;
 import com.sos.joc.classes.JocCockpitProperties;
 import com.sos.joc.classes.WebserviceConstants;
-import com.sos.scheduler.model.SchedulerObjectFactory;
-import com.sos.scheduler.model.objects.Commands;
 
 @Path("/security")
 public class SOSServicePermissionShiro {
@@ -719,9 +717,6 @@ public class SOSServicePermissionShiro {
 
         TimeZone.setDefault(TimeZone.getTimeZone(UTC));
 
-        Globals.schedulerObjectFactory = new SchedulerObjectFactory();
-        Globals.schedulerObjectFactory.initMarshaller(Commands.class);
-        Globals.schedulerObjectFactory.setOmmitXmlDeclaration(true);
         Globals.sosShiroProperties = new JocCockpitProperties();
 
         currentUser = getUserPwdFromHeaderOrQuery(basicAuthorization, user, pwd);
