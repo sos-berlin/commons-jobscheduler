60130760f83e84cbcfc428790fa1b94557a85a39
Uwe Risse <uwe.risse-git@sos-berlin.com>
2017-01-25 11:17:05 +0100

    JITL-327

--- hibernate-commons/src/main/java/com/sos/hibernate/classes/SOSHibernateStatelessConnection.java
+++ hibernate-commons/src/main/java/com/sos/hibernate/classes/SOSHibernateStatelessConnection.java
@@ -6,7 +6,7 @@ public class SOSHibernateStatelessConnection extends SOSHibernateConnection {
 
 	public SOSHibernateStatelessConnection(SOSHibernateFactory factory) {
         super(factory);
-        this.setUseOpenStatelessSession(false);
+        this.setUseOpenStatelessSession(true);
     }
 
 }
