19bce44cefa85dd700519eb8ebbbce47d29b0bf8
SOS-RE <robert.ehrlich@sos-berlin.com>
2017-05-18 12:24:12 +0200

    JITL-346

--- hibernate-commons/src/main/java/com/sos/hibernate/exceptions/SOSHibernateOpenSessionException.java
+++ hibernate-commons/src/main/java/com/sos/hibernate/exceptions/SOSHibernateOpenSessionException.java
@@ -2,6 +2,9 @@ package com.sos.hibernate.exceptions;
 
 import javax.persistence.PersistenceException;
 
+/** if session/connection can't be acquired (for example, database is not running), occur: */
+/** - by openSession() methods if c3p0 not configured */
+/** - if c3p0 configured - occur later in the next methods (for example, by createQuery ... as SOSHibernateQueryException) */
 public class SOSHibernateOpenSessionException extends SOSHibernateException {
 
     private static final long serialVersionUID = 1L;
