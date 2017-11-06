5d087486407c2166df7c1efd120e37983133e27d
SOS-RE <robert.ehrlich@sos-berlin.com>
2017-11-06 12:35:05 +0100

    YADE-446

--- virtual-file-system/src/main/java/com/sos/VirtualFileSystem/DataElements/SOSFileListEntry.java
+++ virtual-file-system/src/main/java/com/sos/VirtualFileSystem/DataElements/SOSFileListEntry.java
@@ -572,10 +572,10 @@ public class SOSFileListEntry extends SOSVfsMessageCodes implements Runnable, IJ
                 String sourceParent = strSourceFileName.substring(0, strSourceFileName.length() - sourceFileName.length());
                 String newSourceFileName = sourceOptions.replacing.doReplace(sourceFileName, replaceWith).replace('\\', '/');
                 if (!newSourceFileName.equals(sourceFileName)) {
-                    String sourceDir = addFileSeparator(sourceOptions.Directory.Value());
+                    String sourceDir = addFileSeparator(sourceOptions.Directory.Value()).replace('\\', '/');
                     if (objOptions.recursive.value()) {
                         String subDirs = sourceParent.substring(sourceDir.length());
-                        newSourceFileName = newSourceFileName.replaceFirst("([^/]*)$", subDirs + "$1");
+                        newSourceFileName = subDirs + newSourceFileName;
                     }
                     if (!newSourceFileName.startsWith("/") && !newSourceFileName.matches("^[a-zA-Z]:[\\\\/].*$")) {
                         newSourceFileName = sourceDir + newSourceFileName;
