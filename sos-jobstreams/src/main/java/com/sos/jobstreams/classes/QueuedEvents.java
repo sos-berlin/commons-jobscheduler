3ef3c8063314bcabc8344440d19b450d2dd1d6a1
Uwe Risse <uwe.risse-git@sos-berlin.com>
2019-09-02 17:48:56 +0200

    JOC-737

--- sos-jobstreams/src/main/java/com/sos/jobstreams/classes/QueuedEvents.java
+++ sos-jobstreams/src/main/java/com/sos/jobstreams/classes/QueuedEvents.java
@@ -3,7 +3,16 @@ package com.sos.jobstreams.classes;
 import com.sos.jobstreams.resolver.JSEvent;
 import com.sos.jobstreams.resolver.JSEvents;
 
+import sos.util.SOSString;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class QueuedEvents extends JSEvents {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(QueuedEvents.class);
+    private static final boolean isDebugEnabled = LOGGER.isDebugEnabled();
+    private static final boolean isTraceEnabled = LOGGER.isTraceEnabled();
 
     public QueuedEvents() {
         super();
@@ -12,6 +21,9 @@ public class QueuedEvents extends JSEvents {
     public void handleAddEventlistBuffer(JSEvents jsEvents) {
         for (JSEvent jsEvent : jsEvents.getListOfEvents().values()) {
             if (jsEvent.isDbError()) {
+                if (isTraceEnabled) {
+                    LOGGER.trace("Adding event into queued events: " + SOSString.toString(jsEvent));
+                }
                 super.addEvent(jsEvent);
             }
         }
@@ -20,6 +32,9 @@ public class QueuedEvents extends JSEvents {
     public void handleDeleteEventlistBuffer(JSEvents jsEvents) {
         for (JSEvent jsEvent : jsEvents.getListOfEvents().values()) {
             if (jsEvent.isDbError()) {
+                if (isTraceEnabled) {
+                    LOGGER.trace("removing event from queued events: " + SOSString.toString(jsEvent));
+                }
                 super.removeEvent(jsEvent);
             }
         }
