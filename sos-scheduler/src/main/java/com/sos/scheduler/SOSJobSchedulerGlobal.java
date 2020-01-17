cae58cd02fecf4e33af03ace8ad3527ef81500e6
uwe-risse-sos <uwe.risse@sos-berlin.com>
2020-01-17 10:57:47 +0100

    JOC-850

--- sos-scheduler/src/main/java/com/sos/scheduler/SOSJobSchedulerGlobal.java
+++ sos-scheduler/src/main/java/com/sos/scheduler/SOSJobSchedulerGlobal.java
@@ -3,6 +3,24 @@ package com.sos.scheduler;
 public class SOSJobSchedulerGlobal {
 
     public static enum JOB_CRITICALITY {
-        NORMAL, MINOR, MAJOR
+        NORMAL {
+
+            public String toString() {
+                return "normal";
+            }
+        },
+        MINOR {
+
+            public String toString() {
+                return "minor";
+            }
+        },
+        MAJOR {
+
+            public String toString() {
+                return "major";
+
+            }
+        }
     }
 }
